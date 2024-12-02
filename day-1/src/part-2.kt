359dea84237edbd849ef7558551ab97700ab8e28
Aasish <aasish23@hotmail.com>
2024-12-01 21:32:04 -0500

    day-1

---
+++ day-1/src/part-2.kt
@@ -0,0 +1,23 @@
+import java.io.File
+
+fun main() {
+//    val input = "day-1/sample.txt"
+    val input = "day-1/input.txt"
+    val lines = File(input).readLines()
+
+    val left = mutableListOf<Int>()
+    val right = mutableMapOf<Int, Int>()
+    for(line in lines) {
+        val pos = line.split("   ").map { it.toInt() }
+        left.add(pos[0])
+        right.put(pos[1], right.getOrDefault(pos[1], 0) + 1)
+    }
+
+    var cnt = 0
+    left.forEach {
+        val curr = right.getOrDefault(it, 0)
+        cnt += (curr * it)
+    }
+
+    println(cnt)
+}
---
+++ day-1/src/part1.kt
@@ -0,0 +1,27 @@
+import java.io.File
+
+fun main() {
+//    val input = "day-1/sample.txt"
+    val input = "day-1/input.txt"
+    val lines = File(input).readLines()
+
+    val left = mutableListOf<Int>()
+    val right = mutableListOf<Int>()
+    for(line in lines) {
+        val pos = line.split("   ").map { it.toInt() }
+        left.add(pos[0])
+        right.add(pos[1])
+    }
+
+    left.sort()
+    right.sort()
+
+    var cnt = 0
+    assert(left.size == right.size)
+
+    left.zip(right).forEach {
+        cnt += Math.abs(it.first - it.second)
+    }
+
+    println(cnt)
+}
